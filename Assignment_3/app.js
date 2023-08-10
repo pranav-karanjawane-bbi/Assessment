e8170125bc82ec4b058fcdfb89fc0a36620dcb78
pranav-karanjawane-bbi <141905198+pranav-karanjawane-bbi@users.noreply.github.com>
2023-08-10 21:16:58 +0530

    Update app.js

--- Assignment_3/app.js
+++ Assignment_3/app.js
@@ -1,15 +1,23 @@
-const rectangles = document.querySelectorAll('.rectangle');
+const rectangle1 = document.getElementById('rectangle1');
+const rectangle2 = document.getElementById('rectangle2');
+const rectangle3 = document.getElementById('rectangle3');
 
-rectangles.forEach(rectangle => {
-    rectangle.addEventListener('click', () => {
-        rectangles.forEach(rect => {
-            if (rect === rectangle) {
-                rect.style.backgroundColor = 'black';
-            } else {
-                rect.style.backgroundColor = 'white';
-            }
-        });
-    });
+rectangle1.addEventListener('click', () => {
+    rectangle1.style.backgroundColor = 'black';
+    rectangle2.style.backgroundColor = 'white';
+    rectangle3.style.backgroundColor = 'white';
+});
+
+rectangle2.addEventListener('click', () => {
+    rectangle1.style.backgroundColor = 'white';
+    rectangle2.style.backgroundColor = 'black';
+    rectangle3.style.backgroundColor = 'white';
+});
+
+rectangle3.addEventListener('click', () => {
+    rectangle1.style.backgroundColor = 'white';
+    rectangle2.style.backgroundColor = 'white';
+    rectangle3.style.backgroundColor = 'black';
 });
 
 
